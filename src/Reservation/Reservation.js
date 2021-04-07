bcaef73ec8d9d49a478d0f38932247d7ae2f3c5c
Jessica Justice <jessbjust@gmail.com>
2021-04-07 10:10:29 -0600

    Polish styling - minor changes

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -6,8 +6,8 @@ const Reservation = ({ id, name, date, time, number }) => {
     <div id={id} className='reservation-card'>
       <h2>{name}</h2>
       <p>{date}</p>
-      <p>{time}</p>
-      <p>{number}</p>
+      <p>{time} pm</p>
+      <p>Number of guests: {number}</p>
       <button className='cancel'>Cancel</button>
     </div>
   )
