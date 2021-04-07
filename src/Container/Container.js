cf8b25881b366b0a2d8a4a7bdacad343e576c000
Jessica Justice <jessbjust@gmail.com>
2021-04-07 10:02:44 -0600

    Add styling to reservation container

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Reservation from '../Reservation/Reservation.js';
+import './Container.css';
 
 const Container = ({ reservations }) => {
   return reservations.map(reservation => {
     return (
       <Reservation
+        key={reservation.id}
         id={reservation.id}
         name={reservation.name}
         date={reservation.date}
