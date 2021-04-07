a02519ca688591651661d6df49846b3278e27ecf
Jessica Justice <jessbjust@gmail.com>
2021-04-07 11:08:06 -0600

    Build tests for page load

--- cypress/integration/reservation_cafe_spec.js
+++ cypress/integration/reservation_cafe_spec.js
@@ -2,9 +2,23 @@ describe('Reservation Cafe', () => {
 
   describe('On Load', () => {
 
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+    });
 
+    it('should have a title', () => {
+      cy.get('h1').contains('Turing Cafe Reservations')
+    });
 
+    it('should have a form', () => {
+      cy.get('form').should('exist');
+    });
 
+    it('should have reservations displayed', () => {
+      for (let i = 1; i <= 9; i++) {
+        cy.get(`div[id=${i}]`).should('exist');
+      }
+    });
   });
 
   describe('Controlled Form', () => {
@@ -22,7 +36,7 @@ describe('Reservation Cafe', () => {
 
 
 
-    
+
   })
 
 
